d13d77e676832424694c51c3cca5feb9fad1874b
xiaoyu <306766053@qq.com>
2016-07-01 20:16:43 +0800

    修改test

--- test/index.js
+++ test/index.js
@@ -15,8 +15,8 @@ let opts = {
   resource: {
     nonce: '4e1f2519c626cbfbab1520c255830c26',
     public: 0,
-    vdoid: 12345
+    vdoid: 12345 //直播结束后保存的文件名
   }
 }
 let rtmpUrl = stream.createRTMPUrl(dName, app, streamName, opts)
-console.log(rtmpUrl)
+console.log(rtmpUrl) //rtmp://blued.uplive.ks-cdn.com/live/1?signature=8WRVCTbwm3MDb9/1soqMt3pmMbo=&accesskey=P3UPCMORAFON76Q6RTNQ&expire=1436976000&nonce=4e1f2519c626cbfbab1520c255830c26&public=0&vdoid=12345
